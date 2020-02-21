5e9681315413cc931825ab503f6723a2283903cc
cww33 <59658864+cww33@users.noreply.github.com>
2020-02-20 23:26:57 -0500

    Update roboadvisor.py

--- roboadvisor.py
+++ roboadvisor.py
@@ -2,6 +2,9 @@
 import requests
 import json
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
@@ -13,7 +16,10 @@ response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2020-02-20"]["4. close"]
+#breakpoint()
 
 
 
@@ -25,8 +31,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
